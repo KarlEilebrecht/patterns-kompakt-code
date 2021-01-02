cf169f861aceed9d4038bb5c925bdab50aeaec6c
Karl Eilebrecht <Karl.Eilebrecht@web.de>
2021-01-02 11:34:56 +0100

    added missing class header comment

--- src/test/java/de/calamanari/pk/muhai/MuhaiGeneratorTest.java
+++ src/test/java/de/calamanari/pk/muhai/MuhaiGeneratorTest.java
@@ -1,3 +1,22 @@
+//@formatter:off
+/*
+ * MuhaiGeneratorTest
+ * Code-Beispiel zum Buch Patterns Kompakt, Verlag Springer Vieweg
+ * Copyright 2014 Karl Eilebrecht
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"):
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//@formatter:on
 package de.calamanari.pk.muhai;
 
 import static de.calamanari.pk.muhai.MuhaiGenerator.IND_SPACER;
