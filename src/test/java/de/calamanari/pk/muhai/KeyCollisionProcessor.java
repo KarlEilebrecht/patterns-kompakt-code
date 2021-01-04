8d0047851cb7fde5313ba53517cbabace2fe7950
Karl Eilebrecht <Karl.Eilebrecht@calamanari.de>
2021-01-04 15:41:14 +0100

    Apfloat code added and translated formula from book

--- src/test/java/de/calamanari/pk/muhai/KeyCollisionProcessor.java
+++ src/test/java/de/calamanari/pk/muhai/KeyCollisionProcessor.java
@@ -25,6 +25,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -37,6 +38,8 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.zip.GZIPInputStream;
 
+import org.apfloat.Apfloat;
+import org.apfloat.ApfloatMath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,14 +51,13 @@ public class KeyCollisionProcessor {
 
     private List<BufferedReader> openChunkReaders = Collections.emptyList();
 
-    public Summary scan(long sizeOfKeyspace, Supplier<Long> keyGenerator, long limit, File outputDir, int maxKeysInMemory, int maxKeysInChunk)
+    public Summary scan(File outputDir, int maxKeysInMemory, int maxKeysInChunk, long sizeOfKeyspace, Supplier<Long> keyGenerator, long limit)
             throws IOException {
 
         LOGGER.info("Phase I: Key generation and chunked storage");
         LOGGER.info("Processing run with {} keys to chunk files at {} ...", limit, outputDir);
         long lastReportedPos = 0;
-        Summary res = new Summary();
-        res.setSizeOfKeyspace(sizeOfKeyspace);
+        SummaryBuilder summaryBuilder = SummaryBuilder.forKeyspaceSizeAndNumberOfKeysGenerated(sizeOfKeyspace, limit);
         List<File> chunkFiles = null;
         try (OrderedChunkFilewriter<KeyAtPos> ocfw = new OrderedChunkFilewriter<>(KeyAtPos.LINE_CODEC, outputDir, "keys-", maxKeysInMemory, maxKeysInChunk)) {
             for (long pos = 0; pos < limit; pos++) {
@@ -68,10 +70,10 @@ public class KeyCollisionProcessor {
             ocfw.flush();
             chunkFiles = ocfw.getChunkFiles();
         }
-        res.setNumberOfKeysGenerated(limit);
         LOGGER.info("{} keys generated", limit);
         LOGGER.info("Phase II: Collision detection");
         LOGGER.info("Merging and iterating over {} chunk files ...", chunkFiles.size());
+        List<File> collisionKeyFiles = Collections.emptyList();
         try (OrderedChunkFilewriter<KeyCollision> ocfw = new OrderedChunkFilewriter<>(KeyCollision.LINE_CODEC, outputDir, "collisions-", maxKeysInMemory,
                 maxKeysInChunk)) {
             chunkFiles.forEach(this::openChunkReader);
@@ -82,28 +84,30 @@ public class KeyCollisionProcessor {
                 ocfw.writeItem(collisionIterator.next());
             }
             ocfw.flush();
-            res.setCollisionKeyFiles(ocfw.getChunkFiles());
+            collisionKeyFiles = ocfw.getChunkFiles();
         }
         finally {
             openChunkReaders.forEach(CloseUtils::closeResourceCatch);
             openChunkReaders.clear();
         }
         chunkFiles.forEach(File::delete);
+
         LOGGER.info("Phase III: Compute collision stats");
-        LOGGER.info("Merging and iterating over {} chunk files ...", res.collisionKeyFiles.size());
+        LOGGER.info("Merging and iterating over {} chunk files ...", collisionKeyFiles.size());
         boolean firstCollision = true;
 
         try {
-            chunkFiles.forEach(this::openChunkReader);
+            collisionKeyFiles.forEach(this::openChunkReader);
             Collection<Iterator<KeyCollision>> chunkIterators = openChunkReaders.stream().map(this::toCollisionIterator).collect(Collectors.toList());
             CombinedOrderedItemIterator<KeyCollision> allCollisionsOrderedIterator = new CombinedOrderedItemIterator<KeyCollision>(chunkIterators);
             long lastCollisionReportedAt = 0;
+            long collidedKeysTotal = 0;
             while (allCollisionsOrderedIterator.hasNext()) {
+                collidedKeysTotal++;
                 KeyCollision collision = allCollisionsOrderedIterator.next();
-                res.addCollisionToStats(collision);
-                if (firstCollision || collision.getPositions()[1] >= lastCollisionReportedAt + 100_000) {
-                    LOGGER.info("Collision detected: {}, {} collisions found in total, {} keys involved", collision, res.numberOfCollisions,
-                            res.numberOfCollidedKeys);
+                summaryBuilder.addCollision(collision);
+                if (limit < 1000 || firstCollision || collision.getPositions()[1] >= lastCollisionReportedAt + 100_000) {
+                    LOGGER.info("Collision detected: {}, total number of collided keys: {}", collision, collidedKeysTotal);
                     lastCollisionReportedAt = collision.getPositions()[1];
                     firstCollision = false;
                 }
@@ -113,8 +117,9 @@ public class KeyCollisionProcessor {
             openChunkReaders.forEach(CloseUtils::closeResourceCatch);
             openChunkReaders.clear();
         }
+        collisionKeyFiles.forEach(File::delete);
 
-        return res;
+        return summaryBuilder.getResult();
     }
 
     private Iterator<KeyCollision> toCollisionIterator(BufferedReader br) {
@@ -147,6 +152,119 @@ public class KeyCollisionProcessor {
     }
 
     /**
+     * Computes the expected number of collisions after creating a number of random keys in a limited keyspace
+     * @param sizeOfKeyspace
+     * @param numberOfKeysGenerated
+     * @return expected number of collisions
+     */
+    static long computeExpectedNumberOfCollisions(long sizeOfKeyspace, long numberOfKeysGenerated) {
+
+        var m = val(sizeOfKeyspace);
+        var one = val(1);
+        var n = val(numberOfKeysGenerated);
+
+        var q = one.subtract(one.divide(m));
+        var qPowN = ApfloatMath.pow(q, n);
+
+        // See formula c(m, n) depicted in the book
+        var c = m.multiply(qPowN.subtract(q)).add(n.subtract(one));
+
+        return c.longValue();
+    }
+
+    /**
+     * Helper method to make code above more readable
+     * @param l (treaded as unsigned integer value)
+     * @return Apfloat with a precision of 500
+     */
+    static Apfloat val(long l) {
+        if (l < 0) {
+            int upper = (int) (l >>> 32);
+            int lower = (int) l;
+
+            BigInteger unsignedBigInteger = (BigInteger.valueOf(Integer.toUnsignedLong(upper))).shiftLeft(32)
+                    .add(BigInteger.valueOf(Integer.toUnsignedLong(lower)));
+            return new Apfloat(unsignedBigInteger, 500);
+        }
+        return new Apfloat(l, 500);
+    }
+
+    private static class SummaryBuilder {
+
+        private Summary result = new Summary();
+
+        static SummaryBuilder forKeyspaceSizeAndNumberOfKeysGenerated(long sizeOfKeyspace, long numberOfKeysGenerated) {
+            SummaryBuilder res = new SummaryBuilder();
+            res.result.setSizeOfKeyspace(sizeOfKeyspace);
+            res.result.setNumberOfKeysGenerated(numberOfKeysGenerated);
+            int numberOfDataPoints = 500;
+            if (numberOfKeysGenerated < numberOfDataPoints) {
+                numberOfDataPoints = (int) numberOfKeysGenerated;
+            }
+            long numberOfKeysPerSlot = numberOfKeysGenerated / numberOfDataPoints;
+            for (int i = 0; i < numberOfDataPoints; i++) {
+                // initalize every slot with the upper bound and the computed expected number of collisions
+                long position = numberOfKeysPerSlot * (i + 1);
+                res.result.getCollisionStats().add(new DataPoint(position, 0L, computeExpectedNumberOfCollisions(sizeOfKeyspace, position)));
+            }
+            return res;
+        }
+
+        SummaryBuilder addCollision(KeyCollision collision) {
+            result.numberOfCollisions = result.numberOfCollisions + collision.getPositions().length - 1;
+            result.numberOfCollidedKeys++;
+
+            Long occurrenceCount = result.multiOccurrenceStats.get(collision.getPositions().length);
+            if (occurrenceCount == null) {
+                occurrenceCount = 1L;
+            }
+            else {
+                occurrenceCount++;
+            }
+            result.multiOccurrenceStats.put(collision.getPositions().length, occurrenceCount);
+
+            createOrUpdateDataPointForCollision(collision);
+            return this;
+        }
+
+        /**
+         * Final action to obtain the result, afterwards this builder becomes invalid
+         * @return summary
+         */
+        Summary getResult() {
+            finalizeDataPoints();
+            Summary res = result;
+            result = null;
+            return res;
+        }
+
+        private void createOrUpdateDataPointForCollision(KeyCollision collision) {
+            List<DataPoint> dataPoints = result.collisionStats;
+            long[] positions = collision.getPositions();
+            // start with the second to (first position is not a collision)
+            for (int i = 1; i < positions.length; i++) {
+                long pos = positions[i];
+                int slot = (int) (pos % ((long) dataPoints.size()));
+                DataPoint dataPoint = dataPoints.get(slot);
+                DataPoint newDataPoint = new DataPoint(dataPoint.position, dataPoint.numberOfCollisionsDetected + 1, dataPoint.numberOfCollisionsExpected);
+                dataPoints.set(slot, newDataPoint);
+            }
+        }
+
+        private void finalizeDataPoints() {
+            List<DataPoint> dataPoints = result.collisionStats;
+            long numberOfCollisionsDetected = dataPoints.get(0).numberOfCollisionsDetected;
+            for (int slot = 1; slot < dataPoints.size(); slot++) {
+                DataPoint current = dataPoints.get(slot);
+                numberOfCollisionsDetected = numberOfCollisionsDetected + current.numberOfCollisionsDetected;
+                DataPoint newDataPoint = new DataPoint(current.position, numberOfCollisionsDetected, current.numberOfCollisionsExpected);
+                dataPoints.set(slot, newDataPoint);
+            }
+        }
+
+    }
+
+    /**
      * result of the collision detection
      */
     public static class Summary {
@@ -159,15 +277,9 @@ public class KeyCollisionProcessor {
 
         private long numberOfCollisions = 0;
 
-        private List<File> collisionKeyFiles = new ArrayList<>();
-
-        private File collisionStatsFile = null;
+        private List<DataPoint> collisionStats = new ArrayList<>();
 
         private Map<Integer, Long> multiOccurrenceStats = new TreeMap<>();
-
-        Summary() {
-            // default constructor
-        }
 
         public long getNumberOfKeysGenerated() {
             return numberOfKeysGenerated;
@@ -193,22 +305,6 @@ public class KeyCollisionProcessor {
             this.numberOfCollisions = numberOfCollisions;
         }
 
-        public List<File> getCollisionKeyFiles() {
-            return collisionKeyFiles;
-        }
-
-        public void setCollisionKeyFiles(List<File> collisionKeyFiles) {
-            this.collisionKeyFiles = collisionKeyFiles;
-        }
-
-        public File getCollisionStatsFile() {
-            return collisionStatsFile;
-        }
-
-        public void setCollisionStatsFile(File collisionStatsFile) {
-            this.collisionStatsFile = collisionStatsFile;
-        }
-
         public Map<Integer, Long> getMultiOccurrenceStats() {
             return multiOccurrenceStats;
         }
@@ -225,23 +321,19 @@ public class KeyCollisionProcessor {
             this.sizeOfKeyspace = sizeOfKeyspace;
         }
 
-        void addCollisionToStats(KeyCollision collision) {
-            numberOfCollisions = numberOfCollisions + collision.getPositions().length - 1;
-            numberOfCollidedKeys++;
-
-            Long occurrenceCount = multiOccurrenceStats.get(collision.getPositions().length);
-            if (occurrenceCount == null) {
-                occurrenceCount = 1L;
-            }
-            else {
-                occurrenceCount++;
-            }
-            multiOccurrenceStats.put(collision.getPositions().length, occurrenceCount);
-
+        public List<DataPoint> getCollisionStats() {
+            return collisionStats;
         }
+
+        public void setCollisionStats(List<DataPoint> collisionStats) {
+            this.collisionStats = collisionStats;
+        }
+
     }
 
     public static class DataPoint {
+
+        public static final String HEADER = "Generated Keys;Detected Collisions;Expected Collisions";
 
         private final long position;
 
@@ -266,6 +358,11 @@ public class KeyCollisionProcessor {
 
         public long getNumberOfCollisionsExpected() {
             return numberOfCollisionsExpected;
+        }
+
+        @Override
+        public String toString() {
+            return "" + position + ";" + numberOfCollisionsDetected + ";" + numberOfCollisionsExpected;
         }
 
     }
--- src/test/java/de/calamanari/pk/muhai/KeyCollisionTest.java
+++ src/test/java/de/calamanari/pk/muhai/KeyCollisionTest.java
@@ -1,5 +1,35 @@
 package de.calamanari.pk.muhai;
 
+import java.math.BigInteger;
+
+import org.apfloat.Apfloat;
+import org.junit.Test;
+
 public class KeyCollisionTest {
+
+    @Test
+    public void testApfloatComputation() {
+
+        Apfloat value = KeyCollisionProcessor.val(Long.MAX_VALUE);
+
+        System.out.println(value.toString());
+
+        System.out.println(MuhaiUtils.toIntString(Long.MIN_VALUE));
+        value = KeyCollisionProcessor.val(Long.MIN_VALUE);
+        System.out.println(value.toString());
+
+        System.out.println(MuhaiUtils.toIntString(-1));
+        value = KeyCollisionProcessor.val(-1);
+        System.out.println(value.toString());
+
+        long m = BigInteger.TWO.pow(32).longValue();
+
+        System.out.println(m);
+
+        for (long n = 1; n < 1_000_002; n = n + 5_000) {
+            System.out.println("" + n + " --> " + KeyCollisionProcessor.computeExpectedNumberOfCollisions(m, n));
+        }
+
+    }
 
 }
